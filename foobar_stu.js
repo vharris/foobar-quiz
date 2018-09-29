25d46317acb1a3db76c914588a6b3c3c583066c9
vharris <vondale.harris@yahoo.com>
2018-09-29 08:47:27 -0500

    student foobar quiz

---
+++ foobar_stu.js
@@ -0,0 +1,13 @@
+/*
+Create a function that prints to console the numbers 1 to 100.
+Replace multiples of 6 with the text "foo" and replace multples of 7 with "bar"
+Replace multiples of 6 AND 7 with "foobar"
+
+Hint: the Modulus operator % might come in handy.
+Bonus: Print the total number of foos, bars, and foobars
+*/
+
+function foobar(){
+// Your code here
+}
+foobar();
